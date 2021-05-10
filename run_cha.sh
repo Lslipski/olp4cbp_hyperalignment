24ca05cca0f68ed53ac62b165d1750265824c9a4
Luke Slipski <f0040y1@ndoli.hpcc.dartmouth.edu>
2021-05-10 08:44:38 -0400

    newbrainmask test in notebook. Last commit before merging to olp4cbp repo

--- run_cha.sh
+++ run_cha.sh
@@ -2,7 +2,7 @@
 #PBS  -m bea
 #PBS -M luke.slipski.gr@dartmouth.edu
 #PBS -N olp4cbp_hyperalign
-#PBS -l nodes=1:ppn=32
+#PBS -l nodes=1:ppn=36
 #PBS -l walltime=350:00:00
 #PBS -o /dartfs-hpc/rc/home/1/f0040y1/CANlab/labdata/projects/OLP4CBP/hyperalignment/log/${PBS_JOBID}.o
 #PBS -e /dartfs-hpc/rc/home/1/f0040y1/CANlab/labdata/projects/OLP4CBP/hyperalignment/log/${PBS_JOBID}.e
@@ -13,4 +13,4 @@ module load python/anaconda2
 
 conda activate comp_meth_env
 
-python /dartfs-hpc/rc/home/1/f0040y1/CANlab/labdata/projects/OLP4CBP/hyperalignment/scripts/run_cha.py 202 10
+python /dartfs-hpc/rc/home/1/f0040y1/CANlab/labdata/projects/OLP4CBP/hyperalignment/scripts/run_cha.py 202 13
