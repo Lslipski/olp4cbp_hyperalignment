14d89661af65921c780065b6c8cd6b0c89add544
Luke Slipski <f0040y1@discovery7.hpcc.dartmouth.edu>
2021-07-26 14:04:48 -0400

    updates to nuisance covs analysis script

--- parcelwise_ha/regressor_analysis/save_all_regressors_nuisance_covs.m
+++ parcelwise_ha/regressor_analysis/save_all_regressors_nuisance_covs.m
@@ -1,8 +1,16 @@
-savepath = '/dartfs-hpc/rc/home/1/f0040y1/CANlab/labdata/projects/OLP4CBP/hyperalignment/parcelwise/analysis/regressors/raw_from_yoni';
+savepath = '/dartfs-hpc/rc/home/1/f0040y1/CANlab/labdata/projects/OLP4CBP/hyperalignment/parcelwise/analysis/regressors';
 addpath(genpath('/dartfs-hpc/rc/home/1/f0040y1/repositories/CanlabCore'));
 addpath(genpath('/dartfs-hpc/rc/home/1/f0040y1/repositories/Neuroimaging_Pattern_Masks'));
 addpath(genpath('/dartfs-hpc/rc/home/1/f0040y1/repositories/spm12'));
 path_to_repos = '/dartfs-hpc/rc/home/1/f0040y1/repositories/';
+
+% Load metadata table for full study
+load(fullfile(path_to_repos, 'OLP4CBP', 'data','subject_metadata.mat'))
+disp('Loaded subject_metadata.mat');
+% select for Time 1 subjects only
+mytabl = tabl(tabl.time==1, :); % maybe also only use patients? is_patient==1. I can't remember what we decided
+
+
 for i = 1:height(mytabl)
     proj_basedir = '/dartfs-hpc/rc/home/1/f0040y1/CANlab/labdata/projects/OLP4CBP/';
     confound_pain_fname = fullfile(proj_basedir, 'first_level', sprintf('sub-%04d', mytabl.id(i)),'ses-01', 'spm_modeling', 'bladder_confounds.mat'); 
