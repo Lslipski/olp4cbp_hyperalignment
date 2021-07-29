a74467ab7b053e0d922e25b69b94ded49088b4a3
Luke Slipski <f0040y1@discovery7.hpcc.dartmouth.edu>
2021-07-29 12:48:03 -0400

    updating prepare group analysis with minimal CHA/AA functionality

--- parcelwise_ha/glm/prepare_group_analysis_object.m
+++ parcelwise_ha/glm/prepare_group_analysis_object.m
@@ -16,28 +16,28 @@ helpdir = '/dartfs-hpc/rc/home/1/f0040y1/CANlab/labdata/projects/OLP4CBP/hyperal
 load(fullfile(scriptdir,'univariate_subject_table.mat'));
 
 % load template fmri_data object
-bladder_pain_aCompCor_antpost_AA = fmri_data(fullfile(helpdir, 'template_nii_in_100105_space.nii'));
-bladder_pain_aCompCor_antpost_AA.metadata_table = univariate_subject_table;
-bladder_pain_aCompCor_antpost_AA.dat = [];
-bladder_pain_aCompCor_antpost_AA.image_names = [];
-bladder_pain_aCompCor_antpost_AA.fullpath = '';
-bladder_pain_aCompCor_antpost_AA.files_exist = logical(repmat(1,size(univariate_subject_table,1),1));
-bladder_pain_aCompCor_antpost_AA.covariate_names = {'Pain Exp Decay VIFs'};
+dat = fmri_data(fullfile(helpdir, 'template_nii_in_100105_space.nii'));
+dat.metadata_table = univariate_subject_table;
+dat.dat = [];
+dat.image_names = [];
+dat.fullpath = '';
+dat.files_exist = logical(repmat(1,size(univariate_subject_table,1),1));
+dat.covariate_names = {'Pain Exp Decay VIFs'};
 
 % load fl_flm files one at a time
 for i = 1:height(univariate_subject_table)
     i
     % load this sub's glm file
-    thisfile = fullfile(fldir, table2array(univariate_subject_table(i, 2)), 'bladder_pain_aCompCor_antpost', strcat('sub-',table2array(univariate_subject_table(i, 2)), '_FL_regressor_individual_model.mat'));
+    thisfile = fullfile(fldir, table2array(univariate_subject_table(i, 2)), 'bladder_pain_aCompCor_antpost', strcat('sub-',table2array(univariate_subject_table(i, 2)), '_FL_regressor_individual_model_CHA.mat'));
     thisglm = load(thisfile);
             
     % copy relevant info to 2nd level object
-    bladder_pain_aCompCor_antpost_AA.dat(:,i) = thisglm.regression_results_ols.b.dat(:,1);
-    bladder_pain_aCompCor_antpost_AA.fullpath(i,:) = thisfile;
-    bladder_pain_aCompCor_antpost_AA.covariates(i) = thisglm.regression_results_ols.diagnostics.Variance_inflation_factors(1);                           
+    dat.dat(:,i) = thisglm.regression_results_ols.b.dat(:,1);
+    dat.fullpath(i,:) = thisfile;
+    dat.covariates(i) = thisglm.regression_results_ols.diagnostics.Variance_inflation_factors(1);                           
 end
 
-dat = preprocess(bladder_pain_aCompCor_antpost_AA, 'smooth', 6);
+bladder_pain_aCompCor_antpost_CHA = preprocess(dat, 'smooth', 6);
 cd(fldir);
-save('bladder_pain_aCompCor_antpost_AA.mat', 'dat', '-v7.3');
+save('bladder_pain_aCompCor_antpost_CHA.mat', 'dat', '-v7.3');
 disp('Saved 2nd level summary file.');
